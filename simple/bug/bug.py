c3da2a628f33cfac5ee7d1a0e5f791a19cb31f3d
LaoFeiYang <kyli.nju@gmail.com>
2020-01-19 11:33:54 -0500

    v3

---
+++ simple/bug/bug.py
@@ -0,0 +1,10 @@
+"""
+A bug that eats moves and reproduces.
+    Bug consumes food keeps size similar as cell
+    Bugs has gender, only female bugs reproduces. There's equal chance of generating a male or a female bug. (think about how to do mix and generate new feature here, maybe start with changing parameters first)
+    Think about how to make some noise for bugs to communicate.
+    Bugs move in a 2d map to search for food. Food generates smell and bug has instinct to get the direction of food.
+    Food can be consumed but new food generates at certain rate.
+"""
+
+
